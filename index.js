a6df4bafd56a8183ff4c19eafe98a0d987e77532
Sanalicyber <omerfaruk.h@outlook.com>
2022-04-24 12:15:58 +0300

    deneme

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var app = express();
 app.use(bodyPars.urlencoded({extended: true}))
 app.use(bodyPars.json())
 
-app.post('/', (req, res) => {
-    res.send("version:1.0.0")
+app.get('/api', (req, res) => {
+    res.send(version)
 })
 
 app.get('/', (req, res) => {
-    res.sendFile("1.0")
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(process.env.PORT || 3000, () => {
